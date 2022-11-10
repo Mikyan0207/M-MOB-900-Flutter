8f9b2498245f487ef0ab613bb8ef5be8b50ccc5f
Florian Theron <mikyan0207@gmail.com>
2022-11-10 10:02:37 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:starlight/presentation/App.dart';
+import 'package:starlight/presentation/app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
