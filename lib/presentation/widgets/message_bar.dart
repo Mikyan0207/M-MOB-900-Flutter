9bbd51ec83aad7900252db90a4a3770582917738
Alexis Wirth <alexis.wirth@outlook.fr>
2022-11-10 15:40:04 +0100

    Fix error linting

--- lib/presentation/widgets/message_bar.dart
+++ lib/presentation/widgets/message_bar.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dropzone/flutter_dropzone.dart';
