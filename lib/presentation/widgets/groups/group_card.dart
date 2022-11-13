08454112bf24847b60bfcb40bb016184476d89f6
Florian Theron <mikyan0207@gmail.com>
2022-11-13 22:35:50 +0100

    Fix Server Icon, Group Card Animation

--- lib/presentation/widgets/groups/group_card.dart
+++ lib/presentation/widgets/groups/group_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:starlight/domain/controllers/group_controller.dart';
@@ -8,7 +9,7 @@ import 'package:starlight/domain/entities/user_entity.dart';
 import 'package:starlight/presentation/themes/theme_colors.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class GroupCard extends StatelessWidget {
+class GroupCard extends StatefulWidget {
   GroupCard({
     super.key,
     required this.group,
@@ -18,26 +19,78 @@ class GroupCard extends StatelessWidget {
   final GroupEntity group;
   final bool isSelected;
 
+  @override
+  State<GroupCard> createState() => _GroupCardState();
+}
+
+class _GroupCardState extends State<GroupCard>
+    with SingleTickerProviderStateMixin {
   final GroupController _groupController = Get.find();
   final HomeController _homeController = Get.find();
   final UserController _authController = Get.find();
+
+  bool _isHovered = false;
+
+  late final AnimationController backgroundColorController;
+  late final Animation<Color?> backgroundColorAnimation;
+
+  late final UserEntity otherUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    otherUser = widget.group.members.firstWhere(
+      (UserEntity element) =>
+          element.id != _authController.currentUser.value.id,
+    );
+
+    backgroundColorController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 125),
+    )..addListener(() {
+        // Marks the widget tree as dirty
+        setState(() {});
+      });
+
+    backgroundColorAnimation = ColorTween(
+      begin: AppColors.black700,
+      end: AppColors.black400,
+    ).animate(backgroundColorController);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
+      onEnter: (PointerEnterEvent event) {
+        if (widget.isSelected ||
+            _groupController.currentGroup.value.id == widget.group.id) {
+          return;
+        }
+
+        backgroundColorController.forward();
+      },
+      onExit: (PointerExitEvent event) {
+        if (widget.isSelected ||
+            _groupController.currentGroup.value.id == widget.group.id) {
+          return;
+        }
+
+        backgroundColorController.reverse();
+      },
       child: GestureDetector(
         onTap: () async {
-          await _groupController.setCurrentGroup(group.id);
+          await _groupController.setCurrentGroup(widget.group.id);
           _homeController.setSelectedTab(AppTab.privateMessage);
         },
         child: Obx(
           () => Container(
             decoration: BoxDecoration(
-              color: isSelected ||
-                      _groupController.currentGroup.value.id == group.id
+              color: widget.isSelected ||
+                      _groupController.currentGroup.value.id == widget.group.id
                   ? AppColors.black400
-                  : Colors.transparent,
+                  : backgroundColorAnimation.value,
               borderRadius: BorderRadius.circular(7.0),
             ),
             child: Padding(
@@ -56,13 +109,7 @@ class GroupCard extends StatelessWidget {
                         height: 60,
                         child: ClipOval(
                           child: Image.network(
-                            group.members
-                                .firstWhere(
-                                  (UserEntity element) =>
-                                      element.id !=
-                                      _authController.currentUser.value.id,
-                                )
-                                .avatar,
+                            otherUser.avatar,
                           ),
                         ),
                       ),
@@ -70,19 +117,12 @@ class GroupCard extends StatelessWidget {
                   ),
                   Expanded(
                     child: Text(
-                      group.name.isNotEmptyAndNotNull
-                          ? group.name
-                          : group.members
-                              .firstWhere(
-                                (UserEntity element) =>
-                                    element.id !=
-                                    _authController.currentUser.value.id,
-                              )
-                              .username,
+                      otherUser.username,
                       style: TextStyle(
-                        color: isSelected ? Vx.white : Vx.gray300,
-                        fontWeight:
-                            isSelected ? FontWeight.w600 : FontWeight.normal,
+                        color: widget.isSelected ? Vx.white : Vx.gray300,
+                        fontWeight: widget.isSelected
+                            ? FontWeight.w600
+                            : FontWeight.normal,
                       ),
                     ),
                   ),
